512f26c3f65da6829ecf8ccb87d9c2635a7bff10
santhakumars12 <santhakumars12@outlook.com>
2023-10-31 17:31:22 +0530

    files modified

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hi, hello vanakamm and Welcome to my app!!!!!!");
+  res.send("Welcome back to my application!!!!!!");
 });
 
 app.get("/info", (req, res) => {
-  res.send("hey this is info api, working correctly");
+  res.send("hey this is info api, working fine! success");
 });
 
 app.listen(80, () => {
-  console.log("listening on 3000");
+  console.log("listening on 80");
 });
